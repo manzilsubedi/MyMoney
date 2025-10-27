f128ce2deda096fa8a66cf6c8ef9eddd33c31c37
Mahesh Subedi <mnzlsubedi@gmail.com>
2025-10-27 22:05:50 +0000

    added nugets

---
+++ MyMoney/Models/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+
+namespace MyMoney.Models.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<MoneyTranscation> Transaction { get; set; }
+
+
+    }
+}
--- MyMoney/Program.cs
+++ MyMoney/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using MyMoney.Models;
+using MyMoney.Models.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresDb")));
+
+//identity setup
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+        options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
